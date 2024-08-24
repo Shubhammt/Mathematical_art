7dbf0fa19ef25c8f856b94c8fad4bbc54788ae5d
Shubhammt <729shubham@gmail.com>
2024-08-24 14:32:03 +0530

    cleaned code

--- Rotating_ellipse.py
+++ Rotating_ellipse.py
@@ -1,26 +1,33 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import matplotlib.animation as animation
+import numpy as np
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(4, 8))
 n = 10000
 theta = np.linspace(0, 400 * np.pi, n)
+
+centre_radius = 0.5
+centre_angular_velocity = 0.04
+minor_axis = 1
+major_axis = 4
+ellipse_angular_velocity = 0.01
+
 def func(theta):
-    x, y = 1*np.cos(theta)+0.5*np.sin(0.04*theta), 4*np.sin(theta)+0.5*np.cos(0.04*theta)
-    w = 0.01
-    x, y = x*np.cos(w*theta) + y*np.sin(w*theta), -x*np.sin(w*theta)+y*np.cos(w*theta)
-    return x, y
+    ellipse_center = [centre_radius*np.sin(centre_angular_velocity*theta), centre_radius*np.cos(centre_angular_velocity*theta)]
+    x_ellipse, y_ellipse = minor_axis*np.cos(theta), major_axis*np.sin(theta)
+    x_wrt_centre, y_wrt_centre = x_ellipse+ellipse_center[0], y_ellipse+ellipse_center[1]
+    x_rot = x_wrt_centre*np.cos(ellipse_angular_velocity*theta) + y_wrt_centre*np.sin(ellipse_angular_velocity*theta)
+    y_rot = -x_wrt_centre*np.sin(ellipse_angular_velocity*theta)+y_wrt_centre*np.cos(ellipse_angular_velocity*theta)
+    return x_rot, y_rot
+
+
 X, Y = func(theta)
-#line, = ax.plot(X[:2], Y[:2], color='blue', alpha=1)
-ax.set(xlim=[-5, 5], ylim=[-5, 5])
+ax.set(xlim=[-5, 5], ylim=[-10, 10])
 ax.axis('off')
 
 def update(frame):
-    #line.set_xdata(X[max(0,frame-5):min(n, frame+5)])
-    #line.set_ydata(Y[max(0,frame-5):min(n, frame+5)])
     ax.plot(X[max(0,frame-5):min(n, frame+5)], Y[max(0,frame-5):min(n, frame+5)], color='blue', alpha=0.1)
     return ax
 
-ani = animation.FuncAnimation(fig=fig, func=update, frames=n, interval=30)
-ani.save(filename="test_2.mp4", writer="ffmpeg", fps = int(60*n/500))
-#plt.show()
+ani = animation.FuncAnimation(fig=fig, func=update, frames=n, interval=30, blit=False)
+ani.save(filename="test_2.mp4", writer="ffmpeg", fps = int(60*n/500), dpi=300)
